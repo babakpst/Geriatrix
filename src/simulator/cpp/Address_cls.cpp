ae6a6e6848a85564737e2b18e667b3dca0c1075d
babakpst <babakpst@gmail.com>
2019-07-31 07:32:52 -0500

    minor spelling error

--- src/simulator/cpp/Address_cls.cpp
+++ src/simulator/cpp/Address_cls.cpp
@@ -2,11 +2,13 @@
 #include "../include/Address_cls.h"
 
 // Constructor
-main_ns::address_ns::address_cls::address_cls()
-{
-  std::cout << " --------------- SeismoOne ---------------" << "\n";
-  std::cout << " ----- Developed by: Babak Poursartip ----" << "\n";
-  std::cout << " -------------- version 3.0 --------------" << "\n";
+main_ns::address_ns::address_cls::address_cls() {
+  std::cout << " --------------- SeismoOne ---------------"
+            << "\n";
+  std::cout << " ----- Developed by: Babak Poursartip ----"
+            << "\n";
+  std::cout << " -------------- version 3.0 --------------"
+            << "\n";
   std::cout << "\n";
 }
 
@@ -15,38 +17,36 @@ main_ns::address_ns::address_cls::address_cls()
 Purpose: This function reads the data file name and directories
 
 Developed by: Babak Poursartip
- 
+
 The Institute for Computational Engineering and Sciences (ICES)
 The University of Texas at Austin
-================================= V E R S I O N ===================================================
+================================= V E R S I O N
+===================================================
 V0.00: 05/14/2018 - Subroutine initiated.
 V0.01: 05/14/2018 - Initiated: Compiled without error for the first time.
 
 ###################################################################################################
 */
 
-void main_ns::address_ns::address_cls::address_fn()
-{
+void main_ns::address_ns::address_cls::address_fn() {
 
   // Open address files
-  std::cout << " -opening the address file ..." << "\n";
+  std::cout << " -opening the address file ..."
+            << "\n";
   std::ifstream Addressfile;
   Addressfile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-  try
-  {
+  try {
     Addressfile.open("Address.txt", std::ios::in);
-  }
-  catch (std::ifstream::failure &inputerr)
-  { // here is the catch for opening the input file.
+  } catch (std::ifstream::failure
+               &inputerr) { // here is the catch for opening the input file.
     std::cout << " Error while opening the address file. \n";
-    std::cout << " Please double check the address file in the cpp folder.\n";
+    std::cout << " Please make sure the input file is in the cpp folder.\n";
     std::cout << " The error message is: " << inputerr.what() << "\n";
   }
 
-
-
   // Reading simulation information
-  std::cout << " -reading the information file ..." << "\n";
+  std::cout << " -reading the information file ..."
+            << "\n";
   getline(Addressfile, TempS);
   Addressfile >> Name; // Input file name
 
@@ -55,11 +55,13 @@ void main_ns::address_ns::address_cls::address_fn()
   Addressfile >> Directory; // Directory of the input file
 
   std::cout << "\n";
-  std::cout << " Analysis information:" << "\n";
+  std::cout << " Analysis information:"
+            << "\n";
   std::cout << " Name:  " << Name << "\n";
   std::cout << " Directory:  " << Directory << "\n";
 
-  std::cout << " -creating directories ..." << "\n";
+  std::cout << " -creating directories ..."
+            << "\n";
 
   // Windows
   /*
@@ -87,6 +89,7 @@ TransferFunction_Dir = Directory + "\\Output\\"  + Name + ".TRF";
   std::cout << "  Output dir:  " << Info_Dir << "\n";
 
   Addressfile.close();
-  std::cout << " -done with the address file, successfully." << "\n"
+  std::cout << " -done with the address file, successfully."
+            << "\n"
             << "\n";
 }
